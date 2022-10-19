e845e605cc2355abf1c6faea8a21aaa3928ab1df
Gmangundo <gmangundo43@gmail.com>
2022-10-19 04:30:04 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char word;
+
+	for (word = 'a'; word <= 'z'; wors++)
+		_putchar(word);
+
+	_putchar('\n');
+}
