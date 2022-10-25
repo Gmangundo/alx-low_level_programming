87feff199a2af3bd8e439aaad19793e3b672115e
Gmangundo <gmangundo43@gmail.com>
2022-10-25 16:13:37 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints other char of a string from line one
+ * @str: string to be used
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int p = 0;
+
+	for (; str[p] != '\0'; p++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[p]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
