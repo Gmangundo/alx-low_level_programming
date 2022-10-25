0a71c333925280419a5bf72a05b267cabfe7731d
Gmangundo <gmangundo43@gmail.com>
2022-10-25 07:04:55 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,11 @@
 /**
  * reset_to_98 - take a pointer and update pointer to 98
  * @n : a pointer
- * Return: 0
+ *
+ * Return: always 0
  */
 
-void void reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
