afdfcd7fadf411b1b7b279d1438cfc74537301f7
Gmangundo <gmangundo43@gmail.com>
2022-10-25 15:50:09 -0700

    task 5 reveerse of a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string -function that reverses a string
+ * @s: string
+ * Return: string in reverse
+ */
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int r = 0;
+	int p;
+
+	while (s[r] != '\0')
+		c++;
+	for (p = 0; i < c; p++)
+	{
+		r--;
+		rv = s[p];
+		s[p] = s[r];
+		s[r] = rv;
+	}
+}
