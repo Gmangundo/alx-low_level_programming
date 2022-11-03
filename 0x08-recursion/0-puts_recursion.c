2da9a2e0294d9475d64fe7a609d703d0301ba904
Gmangundo <gmangundo43@gmail.com>
2022-11-02 20:14:55 -0700

    task 0 print new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a new line followed by new ine
+ *@s: value to print
+ * Return: always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar (*s);
+		_putchar_recursion(s + 1);
+	}
+	if (!*s)
+	_putchar('\n');
+	}
+}
